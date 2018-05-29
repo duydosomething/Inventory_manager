44cadd394d90c8cce30254bbd9f1ede1ccb0ae8b
Duy Tong <linksysqagit@gmail.com>
2018-05-29 07:53:57 -0700

    included conditional modal component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Modal, Button } from 'semantic-ui-react'
 
-const ModalModalExample = () => (
-  <Modal size="small" trigger={<a class="item">Show Modal</a>}>
-    <Modal.Header>Login</Modal.Header>
-    <Modal.Content>
-      <div class="ui input label teal">Email</div>
-        <input type="text" name="email"/>
-      <div class="ui input label teal">Password</div>
-        <input type="text" name="email"/>
-      <div style={{ padding: 10}}>
-        <button class="ui left floated primary button">Submit</button>
-        <button class="ui right floated primary button">Register</button>
-      </div>
+class ModalModalExample extends Component {
+  constructor(props){
+    super(props);
+    this.state = { login: true }
+  }
 
+  handleRegister = () => {
+    this.setState({ login: false });
+  }
 
-    </Modal.Content>
+  handleLogin = () => {
+    this.setState({ login: true });
+  }
 
-  </Modal>
-)
+  render(){
+    this.header = this.state.login ? "Login" : "Register";
+    return (
+      <Modal size="tiny" trigger={<a class="item">Show Modal</a>}>
 
+        <Modal.Header>{this.header}</Modal.Header>
+        <Modal.Content>
+          <div class="ui input label teal">Email</div>
+            <input type="text" name="email"/>
+          <div class="ui input label teal">Password</div>
+            <input type="text" name="email"/>
+          <div style={{ padding: 10}}>
+            <button class="ui left floated primary button">Submit</button>
+            {this.state.login ?
+              <button onClick={this.handleRegister} class="ui right floated primary button">Register</button> :
+              <button onClick={this.handleLogin} class="ui right floated primary button">Login</button>
+            }
+          </div>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
 export default ModalModalExample;
