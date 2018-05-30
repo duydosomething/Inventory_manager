dc36870183b1f9e597af4a0cc24b33f50eded182
Duy Tong <linksysqagit@gmail.com>
2018-05-30 07:32:11 -0700

    changed login/register modal on client side

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,34 +4,58 @@ import { Modal, Button } from 'semantic-ui-react'
 class ModalModalExample extends Component {
   constructor(props){
     super(props);
-    this.state = { login: true }
+    this.state = { login: true,
+                    header: "Login"}
+
   }
 
   handleRegister = () => {
+    this.setState({ header: "Register"})
     this.setState({ login: false });
   }
 
   handleLogin = () => {
+    this.setState({ header: "Login"})
     this.setState({ login: true });
   }
 
-  render(){
-    this.header = this.state.login ? "Login" : "Register";
+  renderToLogin = () => {
     return (
-      <Modal size="tiny" trigger={<a class="item">Show Modal</a>}>
+      <div>Already have account? Please <a onClick={this.handleLogin}>Log in</a></div>
+    )
+  }
 
-        <Modal.Header>{this.header}</Modal.Header>
+  renderToRegister = () => {
+    return (
+      <div>Don't have an account? Please <a onClick={this.handleRegister}>Register</a></div>
+    )
+  }
+  render(){
+    //this.header = this.state.login ? "Login" : "Register";
+    return (
+      <Modal onClose={this.handleLogin} size="tiny" trigger={<a class="item">Login</a>}>
+        <Modal.Header style={{backgroundColor: "#005ce6", color:"white"}}>{this.state.header}</Modal.Header>
         <Modal.Content>
-          <div class="ui input label teal">Email</div>
-            <input type="text" name="email"/>
-          <div class="ui input label teal">Password</div>
-            <input type="text" name="email"/>
-          <div style={{ padding: 10}}>
-            <button class="ui left floated primary button">Submit</button>
+          <div>
+              <form class="ui form">
+                <div class="field">
+                  <label>Email</label>
+                  <input placeholder="Email" />
+                </div>
+                <div class="field">
+                  <label>Password</label>
+                  <input placeholder="Password" />
+                </div>
+                <button type="submit" class="ui button" role="button">Submit</button>
+              </form>
+              <div style={{paddingTop:20}}>
             {this.state.login ?
-              <button onClick={this.handleRegister} class="ui right floated primary button">Register</button> :
-              <button onClick={this.handleLogin} class="ui right floated primary button">Login</button>
+              this.renderToRegister() : this.renderToLogin()
+
+              //<button onClick={this.handleRegister} class="ui right floated primary button">Register</button> :
+              //<button onClick={this.handleLogin} class="ui right floated primary button">Login</button>
             }
+            </div>
           </div>
         </Modal.Content>
       </Modal>
